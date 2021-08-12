a90f2b868564ee5f476bf6f5aea0233b33517bee
Jacob Goldverg <jgoldverg@gmail.com>
2021-08-12 11:17:35 -0400

    initial repo

---
+++ src/main/java/org/onedatashare/odsmetadata/OdsMetadataApplication.java
@@ -0,0 +1,13 @@
+package org.onedatashare.odsmetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsMetadataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdsMetadataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/onedatashare/odsmetadata/OdsMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onedatashare.odsmetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdsMetadataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
