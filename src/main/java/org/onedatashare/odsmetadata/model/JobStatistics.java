7b51406bbe7a40778f2da968240719325244a912
Jacob Goldverg <jgoldverg@gmail.com>
2022-01-07 16:02:16 -0500

    forgot to include Lombok

--- src/main/java/org/onedatashare/odsmetadata/model/JobStatistics.java
+++ src/main/java/org/onedatashare/odsmetadata/model/JobStatistics.java
@@ -1,9 +1,12 @@
 package org.onedatashare.odsmetadata.model;
 
+import lombok.Data;
+
 /**
  * This class represents the statistics regardings a job
  * Some examples are throughput, source, destination, time started, time completed more to come.
  */
+@Data
 public class JobStatistics {
     Status status;
 }
