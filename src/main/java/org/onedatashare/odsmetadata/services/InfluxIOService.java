cb707bf3bb6a0565cf483e4407a20579ae85f4e2
Jacob Goldverg <jgoldverg@gmail.com>
2022-06-28 16:16:53 -0400

    initial Service for Influx

--- src/main/java/org/onedatashare/odsmetadata/services/InfluxIOService.java
+++ src/main/java/org/onedatashare/odsmetadata/services/InfluxIOService.java
@@ -1,2 +1,33 @@
-package org.onedatashare.odsmetadata.services;public class InfluxIOService {
+package org.onedatashare.odsmetadata.services;
+
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.QueryApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class InfluxIOService {
+
+    Logger logger = LoggerFactory.getLogger(InfluxIOService.class);
+
+    @Autowired
+    private InfluxDBClient influxDBClient;
+
+    private QueryApi queryApi;
+
+    private final String defaultTime = "-24h";
+    private final String rangeStatement = "|> range(start: %s)";
+    private final String fromBucket = "from(bucket: \"%s\" ";
+    private final String transferNodeBucket = "ODSTransferNodes";
+    private final String defaultBucket = String.format(fromBucket, transferNodeBucket);
+
+    @PostConstruct
+    public void postConstruct() {
+        this.queryApi = influxDBClient.getQueryApi();
+    }
+
 }
